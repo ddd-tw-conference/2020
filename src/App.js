b840012c1cf4ab7a6e5a9d24951b3cc1b90d1a54
angela <kuanyu861225@gmail.com>
2020-08-05 22:03:53 +0800

    [feature] modify css and remove comments

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React from 'react'
+import logo from './logo.svg'
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
-// import Introduction from './introduction'
+import Introduction from './introduction'
 import Instructor from './instructors/Instructor'
 
 function App() {
   return (
     <div className="App">
       <div>
-        {/*<Introduction />*/}
+        <Introduction />
         <Instructor />
       </div>
     </div>
