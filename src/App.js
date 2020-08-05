e43a2c94c36ff7766eddaad6ed4dbbe4c123cb2c
Sean Lin <seanlin816@gmail.com>
2020-08-05 22:23:45 +0800

    instruction

--- src/App.js
+++ src/App.js
@@ -2,15 +2,15 @@ import React from 'react'
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
-// import Introduction from './introduction'
-import Instructor from './instructors/Instructor'
+import Introduction from './introduction'
+// import Instructor from './instructors/Instructor'
 
 function App() {
   return (
     <div className="App">
       <div>
-        {/*<Introduction />*/}
-        <Instructor />
+        <Introduction />
+        {/*<Instructor />*/}
       </div>
     </div>
   )
