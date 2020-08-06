1653d40243404cb6b136d4de72f8b299f3d04e9a
Sean Lin <seanlin816@gmail.com>
2020-08-06 21:55:16 +0800

    instructor completed

--- src/instructors/Instructor.js
+++ src/instructors/Instructor.js
@@ -1,11 +1,60 @@
 import React from 'react'
+import InstructorSimple from './components/InstructorSimple'
 
 const Instructor = (props) => {
   return (
-    <div>
+    <>
       <div className="container-fluid title">講師介紹</div>
-      <div></div>
-    </div>
+      <div className="container-fluid instructors">
+        <div className="row">
+          <div className="col-4">
+            <InstructorSimple
+              name="Sean Lin"
+              jobTitle="Senior Engineer"
+              imgSrc="/img/sean.png"
+            />
+          </div>
+          <div className="col-4">
+            <InstructorSimple
+              name="Sean Lin"
+              jobTitle="Senior Engineer"
+              imgSrc="/img/sean.png"
+            />
+          </div>
+          <div className="col-4">
+            <InstructorSimple
+              name="Sean Lin"
+              jobTitle="Senior Engineer"
+              imgSrc="/img/sean.png"
+            />
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-4">
+            <InstructorSimple
+              name="Sean Lin"
+              jobTitle="Senior Engineer"
+              imgSrc="/img/sean.png"
+            />
+          </div>
+          <div className="col-4">
+            <InstructorSimple
+              name="Sean Lin"
+              jobTitle="Senior Engineer"
+              imgSrc="/img/sean.png"
+            />
+          </div>
+          <div className="col-4">
+            <InstructorSimple
+              name="Sean Lin"
+              jobTitle="Senior Engineer"
+              imgSrc="/img/sean.png"
+            />
+          </div>
+        </div>
+      </div>
+      <div className="row"></div>
+    </>
   )
 }
 
