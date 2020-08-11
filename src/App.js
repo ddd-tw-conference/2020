72d0337734916282d6fd7b1500d7d99ab53db68e
angela <kuanyu861225@gmail.com>
2020-08-11 12:58:38 +0800

    <feat> add menubar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from 'react'
-import logo from './logo.svg'
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 import Introduction from './introduction'
 import Instructor from './instructors/Instructor'
+import Menubar from './menubar/Menubar'
 
 function App() {
   return (
     <div className="App">
+      <Menubar />
       <div className="container-padding">
         <Introduction />
         <Instructor />
