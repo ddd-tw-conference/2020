b03def51b864ec112907c56004c9d62855076992
VdustR <VdustR@gmail.com>
2020-10-09 14:35:20 +0800

    feat: speaker pages

---
+++ src/pages/speakers/kacper-gunia.tsx
@@ -0,0 +1,30 @@
+import Speaker from "pageContent/Speaker";
+import React, { memo } from "react";
+import useSpeak from "speakers/kacper-gunia/useSpeaker";
+
+function App() {
+  const {
+    topic,
+    content,
+    name,
+    title,
+    introduce,
+    image,
+    ogTitle,
+    ogDescription,
+  } = useSpeak();
+  return (
+    <Speaker
+      topic={topic}
+      content={content}
+      name={name}
+      title={title}
+      introduce={introduce}
+      image={image}
+      ogTitle={ogTitle}
+      ogDescription={ogDescription}
+    />
+  );
+}
+
+export default memo(App);
