6267e06c6b795b60acc4da13f98cad77daf9df9c
VdustR <vdustr@gmail.com>
2021-06-25 19:58:44 +0800

    2020 warning

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,5 +1,13 @@
+import { Button, Link, Snackbar } from "@material-ui/core";
 import { css } from "emotion";
-import React, { memo, ReactNode, StrictMode } from "react";
+import { useIntl } from "gatsby-plugin-intl";
+import React, {
+  memo,
+  ReactNode,
+  StrictMode,
+  useCallback,
+  useState,
+} from "react";
 import ErrorHandler from "./ErrorHandler";
 import Footer from "./Footer";
 import Helmet from "./Helmet";
@@ -13,6 +21,11 @@ const cssOuter = css`
 `;
 
 const Layout = ({ children }: { children?: ReactNode }) => {
+  const { locale } = useIntl();
+  const [warn, setWarn] = useState(true);
+  const dismissWarn = useCallback(() => {
+    setWarn(false);
+  }, []);
   return (
     <StrictMode>
       <Helmet />
@@ -28,6 +41,35 @@ const Layout = ({ children }: { children?: ReactNode }) => {
           </MdxTheme>
         </ErrorHandler>
       </Theme>
+      <Snackbar
+        open={warn}
+        message={
+          locale === "en" ? (
+            <>
+              {
+                "This is the website for conference 2020. You can check the other information "
+              }
+              <Link color="secondary" href="/">
+                {"here"}
+              </Link>
+              {"."}
+            </>
+          ) : (
+            <>
+              {"這裡是 2020 年會官網。可以來"}
+              <Link color="secondary" href="/">
+                {"這裡"}
+              </Link>
+              {"查看其他資訊。"}
+            </>
+          )
+        }
+        action={
+          <Button color="secondary" size="small" onClick={dismissWarn}>
+            {"Dismiss"}
+          </Button>
+        }
+      />
     </StrictMode>
   );
 };
